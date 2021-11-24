320859b4e14e31c298be8b715a82572b106dfc6f
Турбо <ya777007@yandex.ru>
2021-11-24 20:54:52 +0300

    Commit Lesson 2

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,10 +4,30 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "mainapp/index.html")
+    content = {'title':'Магазин'}
+    return render(request, "mainapp/index.html", content)
 
 def products(request):
     return render(request, "mainapp/products.html")
 
 def contact(request):
     return render(request, "mainapp/contact.html")
+
+def context(request):
+    context = {
+        'title':'test context',
+        'header':'Добро пожаловать на сайт',
+        'username':'Джон',
+    }
+    return render(request, "mainapp/contact.html", context)
+
+
+def menu_product(request):
+    links_menu = [
+        {'href':'products_all', 'name':'Всё'},
+        {'href':'products_home', 'name':'Дом'},
+        {'href':'products_office', 'name':'Офис'},
+        {'href':'products_modern', 'name':'Модерн'},
+        {'href':'products_classic', 'name':'Классика'}
+    ]
+    return render(request, 'mainapp/products.html', links_menu)
